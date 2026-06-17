708e410558d0669950797a4b2a44999b0f31eb51
Reecepter <rfreem57@students.kennesaw.edu>
2026-06-17 12:09:30 -0400

    restructuring

--- Assets/Scripts/AreaCompletionTracking.cs
+++ Assets/Scripts/AreaCompletionTracking.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Threading.Tasks.Sources;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AreaCompletionTracking : MonoBehaviour
 {
@@ -12,12 +13,22 @@ public class AreaCompletionTracking : MonoBehaviour
     private int numOfTasksCompleted = 0;
     public TMP_Text scoreText = null;
     private bool tasksCompleted = false;
+    public UnityEvent allHazardsFound;
+    public UnityEvent skipRoom;
     // Start is called before the first frame update
     private void Awake()
     {
         if (scoreText != null)
         {
             UpdateText();
+        }
+        if (allHazardsFound == null)
+        {
+            allHazardsFound = new UnityEvent();
+        }
+        if (skipRoom == null)
+        {
+            skipRoom = new UnityEvent();
         }
     }
     public void TaskCompleted()
@@ -35,10 +46,25 @@ public class AreaCompletionTracking : MonoBehaviour
         if (tasksCompleted == true)
         {
             scoreText.text = areaName.ToString() + ": All Hazards in Area Resolved";
+            allHazardsFound.Invoke();
+            StopCoroutine(RoomTimer());
             return;
         }
         scoreText.text = areaName.ToString() + ": " + numOfTasksCompleted.ToString() + "/" + numOfTasks.ToString() + " Hazards Resolved";
        
 
+    }
+    public void StartRoomTimer()
+    {
+        StartCoroutine(RoomTimer());
+    }
+    IEnumerator RoomTimer()
+    {
+        yield return new WaitForSeconds(420f);
+        
+        if (tasksCompleted != true)
+        {
+            skipRoom.Invoke();
+        }
     }
 }
